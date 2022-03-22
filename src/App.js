6256372b1c5d02b697b118ad7d1c69a630616b37
Pary <81phary@gmail.com>
2022-03-22 17:24:08 +1100

    adding codes for ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useReducer} from 'react';
 
-function App() {
+const initialState = [{id:Date.now(),name:"Phary",email:"phary@gmail.com"}];
+
+const App = () => {
+
+  const reducer = (state,action) => {
+    switch (action.type){
+      case "add" :
+        return [...state,action.payload];
+      case "delete" :
+        return state.filter(contact=>{
+          return contact.id !== action.payload.id;
+        })
+      default:
+        throw new Error();
+    }
+  }
+
+  const [state,dispatch] = useReducer(reducer,initialState);
+  const [name,setName] =  useState("");
+  const [email,setEmail] =  useState("");
+
+
+  const addContact = (e) => {
+      e.preventDefault();
+      setName("");
+      setEmail("");
+      const contact = {
+        id : Date.now(),
+        name,
+        email
+      }
+      dispatch({type:"add",payload:contact})
+  }
+
+
+  // const [counter,setCounter] = useState(0);
+  
+  // const increment = () => {
+  //   setCounter(counter+1);
+  // }
+  
+
+  // const decrement = () => {
+  //   setCounter(counter-1);
+  // }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>MY CONTACT APP</h1>
+      <hr />
+      <form onSubmit={addContact}>
+          <input type='text' placeholder='name' value={name} onChange = {(e)  => setName(e.target.value)} />
+          <input type='text' placeholder='email' value={email} onChange = {(e)  => setEmail(e.target.value)} />
+          <button>Add Contact</button>
+          <br/>
+          <br/>
+          <hr />
+      </form>
+      <div className="container contactList">
+        <h2>My Contact List</h2>
+        <ul>
+          {state.map((contact) => {
+            return (
+                <li key={contact.id}>
+                  <h4><span>{contact.name}</span><hr/><span>{contact.email}</span></h4> 
+                  <button className="btnDelete" onClick={()=>dispatch({type:"delete",payload:{id:contact.id}})}>X</button>
+                </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
